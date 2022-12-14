834d1e8c20e6406b56318382e0e012f93010286d
fewdw <frederic.alefebvre@gmail.com>
2022-12-14 14:10:54 -0500

    new api

---
+++ flask-api/Schema.py
@@ -0,0 +1,12 @@
+from marshmallow import Schema, fields, validate
+
+
+class schemaPost(Schema):
+    id = fields.String(required=True)
+    first_name = fields.String(required=True)
+    last_name = fields.String(required=True)
+    email = fields.String(required=True)
+    gender = fields.String(required=True)
+    department = fields.String(required=True)
+    title = fields.String(required=True)
+    university = fields.String(required=True)
