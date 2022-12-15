1ded6a8b475d4c1455eda6b9b24c89a62d4ff13e
Alexssuto <77691600+Alexssuto@users.noreply.github.com>
2022-12-15 11:14:18 -0500

    update_one() method should update a user by id # Please enter the commit message for your changes. Lines starting

--- flask-api/app.py
+++ flask-api/app.py
@@ -60,6 +60,9 @@ def get_by_id(student_id):
 #Put method
 @app.route('/student/<student_id>', methods=["PUT"])
 def update_student(student_id):
+    db.update_one({'student_id': object(id)},{'$set':{
+        
+    }} )
   
 
 
