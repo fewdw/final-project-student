a089fb8cc5c74d57db5bfaf38b3e03abb2b627bc
Alexssuto <77691600+Alexssuto@users.noreply.github.com>
2022-12-15 14:14:58 -0500

    change the objects

--- flask-api/Schema.py
+++ flask-api/Schema.py
@@ -11,5 +11,13 @@ class schemaPost(Schema):
     department = fields.String(required=True)
     title = fields.String(required=True)
     university = fields.String(required=True)
+
 class schemaPut(schemaPost):
-    complete = fields.Bool(required=True)
+    student_id = fields.String(required=True)
+    first_name = fields.String(required=True)
+    last_name = fields.String(required=True)
+    email = fields.String(required=True)
+    gender = fields.String(required=True)
+    department = fields.String(required=True)
+    title = fields.String(required=True)
+    university = fields.String(required=True)
