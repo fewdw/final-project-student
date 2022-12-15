7754514b459446265d51da9715f08475843e7e02
Alexssuto <77691600+Alexssuto@users.noreply.github.com>
2022-12-15 13:24:51 -0500

    code clean up

--- flask-api/app.py
+++ flask-api/app.py
@@ -25,37 +25,7 @@ def index():
     return 'Hello, this is the main!'
 
 
-data = open('MOCK_DATA.json')
 
-
-@app.route('/student', methods=["POST"])
-def add_student():
-    read = request.json
-    error = schemaPost().validate(read)
-    if error:
-        return error, 400
-
-
-    #Writting to the database
-    addedId = db.results.insert_one(read).inserted_id
-    read["_id"] = str(addedId)
-
-    return jsonify(read)
-
-#Get readings from the database
-@app.route('/student/<student_id>', methods=["GET"])
-def get_by_id(student_id):
-    try:
-        cursor = db.results.find({"student_id": student_id})
-        read = list(cursor)
-        for read in read:
-            if "_id" in read:
-                read["_id"] = str(read["_id"])
-
-                return jsonify(read)
-    except Exception as e:
-        print(e)
-        return {"error": "some error happened"}, 501
 
 
 #Put method
