7e9ef84c46d9332f7a3aa63608e27131692f7854
FrancisShehata <83419373+FrancisShehata@users.noreply.github.com>
2022-12-17 15:46:16 -0500

    Updated POST request

--- flask-api/app.py
+++ flask-api/app.py
@@ -6,30 +6,9 @@ import os
 from Schema import schemaPost
 from bson import json_util
 from bson.objectid import ObjectId
-from controllerHelper.studentControllerHelper.get_students import get_all_students_helper_method, get_one_student_helper_method
-from controllerHelper.studentControllerHelper.delete_students import delete_one_student_helper_method
-from controllerHelper.studentControllerHelper.put_students import put_an_existing_student_helper_method
 from controllerHelper.studentControllerHelper.post_students import post_a_new_student_helper_method
 app = Flask(__name__)
 
-'''
-USE 'flask run' IN TERMNIAL TO START THE API
-'''
-
-#get all students
-@app.route('/students/', methods=['GET'])
-def get_all_students():
-    return get_all_students_helper_method()
-
-#get student by id
-@app.route('/students/<id>', methods=['GET'])
-def get_one_students(id):
-    return get_one_student_helper_method(id)
-
-#delete student by id
-@app.route('/students/', methods=['DELETE'])
-def delete_one_student():
-    return delete_one_student_helper_method(request.json["id"])
 
 #post a new student
 @app.route('/students/<id>', methods=['POST'])
@@ -47,21 +26,6 @@ def post_new_student():
         request.json["projectId"],
         request.json["programming_language"]
     )
-
-#put a new student
-@app.route('/students/', methods=['PUT'])
-def put_an_existing_student():
-    return put_an_existing_student_helper_method(
-        request.json["id"],
-        request.json["first_name"],
-        request.json["last_name"],
-        request.json["email"],
-        request.json["gender"],
-        request.json["professor_name"],
-        request.json["project"],
-        request.json["programming_language"]
-    )
-
 
 
 if __name__ == "__main__":
