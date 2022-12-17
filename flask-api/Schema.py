03a0496e32557cab25e20b566a9b7d564be45b07
FrancisShehata <83419373+FrancisShehata@users.noreply.github.com>
2022-12-17 15:39:20 -0500

    made changes to schema

--- flask-api/Schema.py
+++ flask-api/Schema.py
@@ -2,11 +2,15 @@ from marshmallow import Schema, fields, validate
 
 
 class schemaPost(Schema):
-    id = fields.String(required=True)
+    student_id = fields.Int(required=True)
+    status = fields.Bool(required=True)
     first_name = fields.String(required=True)
     last_name = fields.String(required=True)
     email = fields.String(required=True)
     gender = fields.String(required=True)
-    department = fields.String(required=True)
-    title = fields.String(required=True)
-    university = fields.String(required=True)
+    professor_name = fields.String(required=True)
+    year_of_graduation = fields.Int(required=True)
+    degree = fields.String(required=True)
+    projectId = fields.String(required=True)
+    programming_language = fields.String(required=True)
+    
