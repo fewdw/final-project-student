d35dcf57c7681313f33edc226cd36b4f0094ed7a
Alexssuto <77691600+Alexssuto@users.noreply.github.com>
2022-12-17 15:54:04 -0500

    connection string

--- flask-api/controllerHelper/studentControllerHelper/put_students.py
+++ flask-api/controllerHelper/studentControllerHelper/put_students.py
@@ -8,3 +8,4 @@ from bson import json_util
 from bson.objectid import ObjectId
 
 MONGODB_PASS = os.environ.get("MONGODB_PASS")
+connection_string = f"mongodb+srv://fewdw:{MONGODB_PASS}@student.kslusvd.mongodb.net/?retryWrites=true&w=majority"
