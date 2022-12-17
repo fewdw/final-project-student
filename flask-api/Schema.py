9ccdfe8656300e816f6cefb773788d551fe88a9a
FrancisShehata <83419373+FrancisShehata@users.noreply.github.com>
2022-12-17 14:39:39 -0500

    Changed schema to match the fields

--- flask-api/Schema.py
+++ flask-api/Schema.py
@@ -3,10 +3,13 @@ from marshmallow import Schema, fields, validate
 
 class schemaPost(Schema):
     id = fields.String(required=True)
+    status = fields.Bool(required=True)
     first_name = fields.String(required=True)
     last_name = fields.String(required=True)
     email = fields.String(required=True)
     gender = fields.String(required=True)
-    department = fields.String(required=True)
-    title = fields.String(required=True)
-    university = fields.String(required=True)
+    professor_name = fields.String(required=True)
+    year_of_graduation = fields.Int(required=True)
+    degree = fields.String(required=True)
+    projectId = fields.String(required=True)
+    programming_language = fields.String(required=True)
