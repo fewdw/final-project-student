17f26dff0a794427e1bea89f759fdd6bbd16e40e
Your Name <frederic.alefebvre@gmail.com>
2022-12-17 14:36:47 -0500

    fixed get and delete

--- flask-api/app.py
+++ flask-api/app.py
@@ -1,62 +1,46 @@
-import pymongo as pymongo
 from flask import Flask, request, jsonify, abort
+import pymongo as pymongo
+from pymongo import MongoClient
 import json
 import os
 from Schema import schemaPost
+from bson import json_util
+from bson.objectid import ObjectId
 app = Flask(__name__)
 
+from controllerHelper.studentControllerHelper.get_students import get_all_students_helper_method, get_one_student_helper_method
+from controllerHelper.studentControllerHelper.delete_students import delete_one_student_helper_method
+from controllerHelper.studentControllerHelper.put_students import put_an_existing_student_helper_method
+from controllerHelper.studentControllerHelper.post_students import post_a_new_student_helper_method
+
 
 '''
-
 USE 'flask run' IN TERMNIAL TO START THE API
-
 '''
 
-MONGODB_LINK = os.environ.get("MONGODB_LINK")
-MONGODB_USER = os.environ.get("MONGODB_USER")
-MONGODB_PASS = os.environ.get("MONGODB_PASS")
-
-client = pymongo.MongoClient(f"mongodb://{MONGODB_USER}:{MONGODB_PASS}@{MONGODB_LINK}/?retryWrites=true&w=majority")
-db = client.test
-
-@app.route('/')
-def index():
-    return 'Hello, Flask!'
+#get all students
+@app.route('/students/', methods=['GET'])
+def get_all_students():
+    return get_all_students_helper_method()
 
 
-data = open('MOCK_DATA.json')
+#get student by id
+@app.route('/students/<id>', methods=['GET'])
+def get_all_students(id):
+    return get_one_student_helper_method(id)
 
-# GET
+#delete student by id
+@app.route('/students/', methods=['DELETE'])
+def delete_one_student():
+    return delete_one_student_helper_method(request.json["id"])
+
+#post a new student
+#@app.route('/students/', methods=['POST'])
 
 
-@app.route('/student', methods=["POST"])
-def add_student():
-    read = request.json
-    error = schemaPost().validate(read)
-    if error:
-        return error, 400
+#put a new student
 
 
-    #Writting to the database
-    addedId = db.results.insert_one(read).inserted_id
-    read["_id"] = str(addedId)
-
-    return jsonify(read)
-
-#Get readings from the database
-@app.route('/student/<student_id>', methods=["GET"])
-def get_by_id(student_id):
-    try:
-        cursor = db.results.find({"student_id": student_id})
-        read = list(cursor)
-        for read in read:
-            if "_id" in read:
-                read["_id"] = str(read["_id"])
-
-                return jsonify(read)
-    except Exception as e:
-        print(e)
-        return {"error": "some error happened"}, 501
 
 
 if __name__ == "__main__":
--- flask-frontend/app.py
+++ flask-frontend/app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 def index():
     return render_template("index.html", name="hello")
 
+
 if __name__ == '__main__':
     app.run()
 
