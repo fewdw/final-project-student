d1a8a2e54e3d8ac70d89a4453ca74302d85fd527
Your Name <frederic.alefebvre@gmail.com>
2022-12-17 15:24:48 -0500

    mongo connection

---
+++ flask-api/controllerHelper/studentControllerHelper/delete_students.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, jsonify, abort
+import pymongo as pymongo
+from pymongo import MongoClient
+import json
+import os
+from Schema import schemaPost
+from bson import json_util
+from bson.objectid import ObjectId
+
+MONGODB_PASS = os.environ.get("MONGODB_PASS")
+connection_string = f"mongodb+srv://fewdw:{MONGODB_PASS}@student.kslusvd.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(connection_string)
+database = client.StudentDB
+student_collection = database.StudentCollection
+
