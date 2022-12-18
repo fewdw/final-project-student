122baeecae57a415962b73a5a6613644c0815fa3
VictorSsuto <83074897+VictorSsuto@users.noreply.github.com>
2022-12-17 21:34:06 -0500

    Created the connection String

--- flask-api/controllerHelper/studentControllerHelper/get_students.py
+++ flask-api/controllerHelper/studentControllerHelper/get_students.py
@@ -6,3 +6,9 @@ import os
 from Schema import schemaPost
 from bson import json_util
 from bson.objectid import ObjectId
+
+MONGODB_PASS = os.environ.get("MONGODB_PASS")
+connection_string = f"mongodb+srv://fewdw:{MONGODB_PASS}@student.kslusvd.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(connection_string)
+database = client.StudentDB
+student_collection = database.StudentCollection
