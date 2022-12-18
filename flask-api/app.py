3873fd1004fc9aa502f9b7e2c45d2fd0f21ad5a0
Alexssuto <77691600+Alexssuto@users.noreply.github.com>
2022-12-17 22:02:35 -0500

    clean up

--- flask-api/app.py
+++ flask-api/app.py
@@ -6,28 +6,21 @@ import os
 from Schema import schemaPost
 app = Flask(__name__)
 
-from controllerHelper.studentControllerHelper.get_students import get_all_students_helper_method, get_one_student_helper_method
-from controllerHelper.studentControllerHelper.delete_students import delete_one_student_helper_method
+
 from controllerHelper.studentControllerHelper.put_students import put_an_existing_student_helper_method
-from controllerHelper.studentControllerHelper.post_students import post_a_new_student_helper_method
+
 
 
 '''
 USE 'flask run' IN TERMNIAL TO START THE API
 '''
 
-#get all students
-@app.route('/students/', methods=['GET'])
-def get_all_students():
-    return get_all_students_helper_method()
+
 
 client = pymongo.MongoClient(f"mongodb://{MONGODB_USER}:{MONGODB_PASS}@{MONGODB_LINK}/?retryWrites=true&w=majority")
 db = client.StudentDB
 
-#delete student by id
-@app.route('/students/', methods=['DELETE'])
-def delete_one_student():
-    return delete_one_student_helper_method(request.json["id"])
+
 
 #put student by id 
 @app.route('/students/', methods=['PUT'])
