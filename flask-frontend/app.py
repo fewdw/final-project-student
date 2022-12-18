415b18bae28f42e120070642fcdc08eb3b3d3667
Your Name <frederic.alefebvre@gmail.com>
2022-12-18 00:53:14 -0500

    url bug

--- flask-frontend/app.py
+++ flask-frontend/app.py
@@ -3,7 +3,7 @@ import requests, json
 
 app = Flask(__name__)
 
-student_list = "http://127.0.0.1:5001/students/"
+student_list = "http://127.0.0.1:5000/students/"
 
 # get all students
 response = requests.get(student_list)
