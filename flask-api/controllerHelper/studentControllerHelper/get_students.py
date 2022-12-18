d62bccf8072343044505119d9bae72ccd0494f99
VictorSsuto <83074897+VictorSsuto@users.noreply.github.com>
2022-12-17 21:34:49 -0500

    Get by ID

--- flask-api/controllerHelper/studentControllerHelper/get_students.py
+++ flask-api/controllerHelper/studentControllerHelper/get_students.py
@@ -17,3 +17,8 @@ student_collection = database.StudentCollection
 def get_all_students_helper_method():
     all_students = student_collection.find()
     return json.loads(json_util.dumps(all_students))
+
+# get by id
+def get_one_student_helper_method(student_id):
+    student = student_collection.find_one({"_id":ObjectId(student_id)})
+    return json.loads(json_util.dumps(student))
