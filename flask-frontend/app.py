3dc941cd68d1f10d8503be858dc6a96a5ea2ae4c
FrancisShehata <83419373+FrancisShehata@users.noreply.github.com>
2022-12-19 12:00:23 -0500

    changes

--- flask-frontend/app.py
+++ flask-frontend/app.py
@@ -79,7 +79,7 @@ def student_added_from_form():
     request.form.get("last_name")
     request.form.get("email")
     request.form.get("gender")
-    request.form.get("student_Id")
+    request.form.get("professor_name")
     request.form.get("student_Id")
     request.form.get("student_Id")
     request.form.get("student_Id")
