20d9028c8c6f90a3bb35c8c1523c88d12bba9aab
FrancisShehata <83419373+FrancisShehata@users.noreply.github.com>
2022-12-19 12:06:19 -0500

    changes

--- flask-frontend/student_api_request.py
+++ flask-frontend/student_api_request.py
@@ -27,3 +27,4 @@ def delete_student_from_api(id):
     def add_student_to_list(student_Id,status,first_name,last_name,email,gender, professor_name, year_of_graduation, degree, projectId, programming_language):
         url = "http://127.0.0.1:5001/admin/list"
         payload = {"studentId":student_Id,"status":status,"firstName":first_name, "last_name":last_name, "email":email, "gender":gender,
+        "professor_name":professor_name
