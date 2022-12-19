4f90b656e031e2f6075d41dce204cb471d36c269
Alexssuto <77691600+Alexssuto@users.noreply.github.com>
2022-12-19 11:53:27 -0500

    all fields called

--- flask-frontend/student_api_request.py
+++ flask-frontend/student_api_request.py
@@ -26,5 +26,16 @@ def delete_student_from_api(id):
 # put one student
 def update_student_from_api(id,student_Id, status, first_name, last_name, email, gender, professor_name, year_of_graduation, degree, projectId, programming_language):
     _id = id[10:34]
+    student_Id = student_Id
+    status = status
+    first_name = first_name
+    last_name = last_name
+    email = email
+    gender = gender
+    professor_name = professor_name
+    year_of_graduation = year_of_graduation
+    degree = degree
+    projectId = projectId
+    programming_language
     all_students_api_link = "http://127.0.0.1:5001/students/"
     response = requests.get(all_students_api_link)
