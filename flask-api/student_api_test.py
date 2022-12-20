7465dd202ae5be2a6ed222d2b18c26ed94c87754
Alexssuto <77691600+Alexssuto@users.noreply.github.com>
2022-12-20 02:18:32 -0500

    Update student_api_test.py
    put method payload

--- flask-api/student_api_test.py
+++ flask-api/student_api_test.py
@@ -1,1 +1,24 @@
+ # put test
+    def test_put_should_work(self):
+        url = "http://127.0.0.1:5000/students/"
+        payload = {
+            "id": "63a13a5da4acd21e65360820",
+            "student_id":"UPDATEDupdated-student_id",
+            "status" : "UPDATEDstatus",
+            "first_name" : "UPDATEDfirst_name",
+            "last_name" : "UPDATEDlast_name",
+            "email" : "UPDATEDemail",
+            "gender" :  "UPDATEDgender",
+            "professor_name" : "UPDATEDprofessor_name",
+            "year_of_graduation" : "UPDATEDyear_of_graduation",
+            "degree" : "degree",
+            "projectId" : "projectId",
+            "programming_language" : "UPDATEDprogramming_language"    
+        }
+        put_response = requests.put(url, json=payload) 
+        self.assertEqual(put_response.json()['updated student with id'],"63a13a5da4acd21e65360820")
 
+
+
+if __name__ == '__main__':
+    unittest.main()
