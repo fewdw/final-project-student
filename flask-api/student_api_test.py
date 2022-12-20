fddfe8028553a0beb1440ce3af337ec9dca32464
fal <54687030+fewdw@users.noreply.github.com>
2022-12-20 02:29:32 -0500

    Update student_api_test.py

--- flask-api/student_api_test.py
+++ flask-api/student_api_test.py
@@ -15,16 +15,51 @@ class TestDefaultRoute(unittest.TestCase):
     def test_default_route_200(self):
         response = app.test_client().get('/')
         self.assertEqual(response.status_code, 200)
-# get by id
+
+    # get by id
     def test_students_route_get_by_id(self):
         response = requests.get('http://127.0.0.1:5000/students/63a1453dd4c253b8774b74b4')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json()['first_name'],'Agnes')
+
     # get all
     def test_students_route_get_all(self):
         response = requests.get('http://127.0.0.1:5000/students')
         self.assertGreater(len(response.json()),1,"test")
-# put test
+
+    # post should return 400
+    def test_students_route_get_all_should_return_400(self):
+        response = requests.post('http://127.0.0.1:5000/students',data={"not":"valid"})
+        self.assertEqual(response.status_code, 400)
+
+    # post and delete should work
+    def test_students_route_get_all_should_work(self):
+        url = "http://127.0.0.1:5000/students"
+        payload = {
+        "student_id":"student_Id",
+        "status":True,
+        "first_name":"first_name",
+        "last_name":"last_name",
+        "email":"email", 
+        "gender":"gender",
+        "professor_name":"professor_name", 
+        "year_of_graduation": "year_of_graduation", 
+        "degree":"degree",
+        "projectId":"projectId",
+        "programming_language":"programming_language"}
+        response = requests.post(url, json=payload)
+        j = response.json()
+        _id_to_delete = j['added']
+        self.assertEqual(response.status_code, 200)
+        
+        self.assertIsNotNone(response.json()['added'])
+
+        url = "http://127.0.0.1:5000/students/"
+        payload = {"id":_id_to_delete}
+        del_response = requests.delete(url, json=payload)
+        self.assertEqual(del_response.json(),{'successfully deleted student with id': _id_to_delete})
+
+    # put test
     def test_put_should_work(self):
         url = "http://127.0.0.1:5000/students/"
         payload = {
