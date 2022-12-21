cfc74011ef593571cc7d82b9db3f78cb03e0d10d
Your Name <frederic.alefebvre@gmail.com>
2022-12-21 01:22:58 -0500

    added mac certificate

--- flask-api/controllerHelper/studentControllerHelper/delete_students.py
+++ flask-api/controllerHelper/studentControllerHelper/delete_students.py
@@ -5,10 +5,12 @@ import json
 import os
 from bson import json_util
 from bson.objectid import ObjectId
+import certifi
+ca = certifi.where()
 
 MONGODB_PASS = os.environ.get("MONGODB_PASS")
 connection_string = f"mongodb+srv://fewdw:{MONGODB_PASS}@student.kslusvd.mongodb.net/?retryWrites=true&w=majority"
-client = MongoClient(connection_string)
+client = MongoClient(connection_string, tlsCAFile=ca)
 database = client.StudentDB
 student_collection = database.StudentCollection
 
--- flask-api/controllerHelper/studentControllerHelper/put_students.py
+++ flask-api/controllerHelper/studentControllerHelper/put_students.py
@@ -5,12 +5,14 @@ import json
 import os
 from bson import json_util
 from bson.objectid import ObjectId
+import certifi
+ca = certifi.where()
 
 MONGODB_PASS = os.environ.get("MONGODB_PASS")
 connection_string = f"mongodb+srv://fewdw:{MONGODB_PASS}@student.kslusvd.mongodb.net/?retryWrites=true&w=majority"
-client = MongoClient(connection_string)
+client = MongoClient(connection_string, tlsCAFile=ca)
 database = client.StudentDB
-student_collection = database.studentCollection
+student_collection = database.StudentCollection
 
 def put_an_existing_student_helper_method(id,student_id, status, first_name, last_name, email, gender, professor_name, year_of_graduation, degree, projectId, programming_language):
     filter = {"_id":ObjectId(id)}
