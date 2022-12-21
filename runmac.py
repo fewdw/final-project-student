f3dea11dbc47d85537c38fde34b58608731ef461
fal <54687030+fewdw@users.noreply.github.com>
2022-12-21 01:41:21 -0500

    Delete runmac.py

--- runmac.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-subprocess.run(["cd", "flask-api"])
-subprocess.Popen(["flask", "run", "--port", "5001"], shell=True)
-subprocess.run(["cd", ".."])
-subprocess.run(["cd", "flask-frontend"])
-subprocess.Popen(["flask", "run"], shell=True)
