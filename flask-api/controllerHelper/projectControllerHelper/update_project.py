ede839ed518b8c92d2b7880b9f246b8f3a95d581
Alexssuto <77691600+Alexssuto@users.noreply.github.com>
2023-01-10 14:33:04 -0500

    database set up

--- flask-api/controllerHelper/projectControllerHelper/update_project.py
+++ flask-api/controllerHelper/projectControllerHelper/update_project.py
@@ -7,3 +7,10 @@ from bson import json_util
 from bson.objectid import ObjectId
 import certifi
 ca = certifi.where()
+
+
+MONGODB_PASS = os.environ.get("MONGODB_PASS")
+connection_string = f"mongodb+srv://fewdw:{MONGODB_PASS}@student.kslusvd.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(connection_string, tlsCAFile=ca)
+database = client.StudentDB
+degree_collection = database.DegreeCollection
