8c87fb77b7218da4ea5008dec21bf2e92428b207
Alexssuto <77691600+Alexssuto@users.noreply.github.com>
2023-01-09 21:38:28 -0500

    delete

---
+++ flask-api/controllerHelper/projectControllerHelper/delete_project.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify, abort
+import pymongo as pymongo
+from pymongo import MongoClient
+import json
+import os
+from bson import json_util
+from bson.objectid import ObjectId
+import certifi
+ca = certifi.where()
+
+MONGODB_PASS = os.environ.get("MONGODB_PASS")
+connection_string = f"mongodb+srv://fewdw:{MONGODB_PASS}@student.kslusvd.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(connection_string, tlsCAFile=ca)
+database = client.StudentDB
+project_collection = database.ProjectCollection
+
+# delete by id
+def delete_one_project_helper_method(project_id):
+    degree = project_collection.delete_one({"_id":ObjectId(project_id)})
+    return jsonify({"msg":"degree successfully deleted"})
