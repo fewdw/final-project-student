83b438fb5b2d0ad0ca4f9a47fe2763d139e3fe7c
FrancisShehata <83419373+FrancisShehata@users.noreply.github.com>
2023-01-10 13:08:37 -0500

    changes

--- flask-api/controllerHelper/studentProjectHelper/get_project.py
+++ flask-api/controllerHelper/studentProjectHelper/get_project.py
@@ -22,6 +22,6 @@ def get_all_projects_helper_method():
     all_projects = project_collection.find()
     return json.loads(json_util.dumps(all_projects))
 
-def get_project_by_id(degree_id):
+def get_project_by_id(project_id):
     degree = project_collection.find_one({"_id":ObjectId(degree_id)})
     return json.loads(json_util.dumps(degree))
