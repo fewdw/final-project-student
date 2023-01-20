34a27ac8f4ef3d4da369bdc0b50ce9af1f08b532
fred <frederic.alefebvre@gmail.com>
2023-01-20 13:45:25 -0500

    added functionality for admin to update a student degree

--- flask-frontend/static/scipts.js
+++ flask-frontend/static/scipts.js
@@ -1,3 +1,12 @@
-function updateStudentDegree(){
-    alert("update button clicked for student degree")
+function updateStudentDegree(name,desc,id){
+    let degreeNameInputField = document.getElementById("degreeNameInputField");
+    let degreeDescriptionInputField = document.getElementById("degreeDescriptionInputField");
+    let degreeIdFormInput = document.getElementById("degreeIdFormInput");
+
+    degreeIdFormInput.value = id.toString();
+    degreeNameInputField.value = name.toString();
+    degreeDescriptionInputField.value = desc.toString();
 }
+
+
+//enter function to update project here.
