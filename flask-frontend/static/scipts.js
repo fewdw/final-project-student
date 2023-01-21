90232e861f6b614b4b39b8e0b4c23f924e93de5c
Alexssuto <77691600+Alexssuto@users.noreply.github.com>
2023-01-21 13:20:22 -0500

    fuctions

---
+++ flask-frontend/static/scipts.js
@@ -0,0 +1,21 @@
+function updateStudentDegree(name,desc,id){
+    let degreeNameInputField = document.getElementById("degreeNameInputField");
+    let degreeDescriptionInputField = document.getElementById("degreeDescriptionInputField");
+    let degreeIdFormInput = document.getElementById("degreeIdFormInput");
+
+    degreeIdFormInput.value = id.toString();
+    degreeNameInputField.value = name.toString();
+    degreeDescriptionInputField.value = desc.toString();
+}
+
+
+function updateStudentProject(name,desc,id){
+    let projectNameInputField = document.getElementByIdById("projectNameInputField");
+    let projectDescriptionInputField = document.getElementById("projectDescriptionInputField");
+    let projectIdFromInput = document.getElementById("projectIdFromInput");
+
+    
+
+
+}
+//enter function to update project here.
