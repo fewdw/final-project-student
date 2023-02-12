7b3c172b2d31faf9618121c0dfab2f152c4fb8ef
fal <54687030+fewdw@users.noreply.github.com>
2023-02-11 19:35:38 -0500

    email functionality

--- flask-frontend/emailer.py
+++ flask-frontend/emailer.py
@@ -1,20 +1,27 @@
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import smtplib
+import os
 
-def send_mail(employer_email):
+smtp_email = os.environ.get("smtp_email")
+smtp_password = os.environ.get("smtp_password")
+
+def send_mail(employer_email,student):
+
+    message = " "
+
     try:
         message = MIMEMultipart()
         message["from"] = "ISRS Team"
         message["to"] = employer_email
         message["subject"] = "Student Information from ISRS"
-        message.attach(MIMEText("Body"))
+        message.attach(MIMEText(str(student)))
 
         with smtplib.SMTP("smtp.gmail.com", port=587) as smtp:
             smtp.ehlo()
             smtp.starttls()
-            smtp.login("isrsteam19@gmail.com","enqxoehwrjwwnkdq")
+            smtp.login(smtp_email,smtp_password)
             smtp.send_message(message)
-            print("Sent!")
+            return True
     except:
-        print("Couldn't send email to ",employer_email)
+        return False
